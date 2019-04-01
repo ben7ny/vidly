ac4ffb4d80638a8c7f47a119e3fcfd63a5200f2e
Ben Jason <benjason777@gmail.com>
2019-04-01 15:49:48 -0700

    Basic is set

---
+++ src/components/movies.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies: movies });
+  };
+
+  render() {
+    const movieCount = this.state.movies.length;
+    if (movieCount === 0) return <p>There is no movie</p>;
+    return (
+      <>
+        <p>the are {this.state.movies.length} avalable</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.Genre}</td>
+                <td>{movie.Stock}</td>
+                <td>{movie.Rate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
+
+export default Movies;
