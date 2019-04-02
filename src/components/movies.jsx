5addcbd431be008c442b4e82719a0c8663355802
Ben Jason <benjason777@gmail.com>
2019-04-02 15:00:17 -0700

    added utils foleder and deployed paginatefunc

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Like from "./common/like";
 import Pagination from "./common/pagination";
 import { getMovies } from "../services/fakeMovieService";
+import { paginate } from "../utils/paginate";
 
 class Movies extends Component {
   state = {
@@ -30,7 +31,8 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage } = this.state;
+    const { pageSize, currentPage, movies } = this.state;
+    const moviesOnPage = paginate(movies, currentPage, pageSize);
     if (count === 0) return <p>There is no movie</p>;
     return (
       <>
@@ -47,7 +49,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map(movie => (
+            {moviesOnPage.map(movie => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
