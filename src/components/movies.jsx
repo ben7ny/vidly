9cd4a1dbd490ab4da61bfd3a79d60b15058e8602
Ben Jason <benjason777@gmail.com>
2019-04-01 18:29:55 -0700

    add common like component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Like from "./common/like";
 import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
@@ -9,6 +10,14 @@ class Movies extends Component {
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
     this.setState({ movies: movies });
+  };
+
+  handleLike = movie => {
+    const movies = [...this.state.movies]; //clone the whole movies array so it doesn't change
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] }; //clone the object inside the movies array so it doesn't change
+    movies[index].liked = !movies[index].liked; // switch it(by default is undefined or falsy) is not in the object yet
+    this.setState({ movies });
   };
 
   render() {
@@ -25,6 +34,7 @@ class Movies extends Component {
               <th>Stock</th>
               <th>Rate</th>
               <th />
+              <th />
             </tr>
           </thead>
           <tbody>
@@ -34,6 +44,13 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
+
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
