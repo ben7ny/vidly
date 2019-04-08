3a39d4bf91e3b1e9ac33b655bdaa86f144f504c5
Ben Jason <benjason777@gmail.com>
2019-04-08 11:23:06 -0700

    the code on the movie compo needs refactoring

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -1,17 +1,8 @@
 import React from "react";
 import TableHeader from "./tableHeader";
 import TableBody from "./tableBody";
-
-const Table = props => {
-  const {
-    moviesOnPage,
-    onDelete,
-    onLike,
-    onSort,
-    sortColumn,
-    columns,
-    data
-  } = props;
+// instead of pass the prop and destructuring, you can destructure it in the argument
+const Table = ({ onSort, sortColumn, columns, data }) => {
   return (
     <table className="table">
       <TableHeader columns={columns} onSort={onSort} sortColumn={sortColumn} />
