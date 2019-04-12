4c5df32730a9b8d384812f2c3a99a8e4a145416d
Ben Jason <benjason777@gmail.com>
2019-04-11 17:33:15 -0700

    added basic validation to login form

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // look at the interface { name, label, value, onChange }
-const Input = ({ name, label, value, onChange }) => {
+const Input = ({ name, label, value, onChange, error }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
@@ -12,6 +12,8 @@ const Input = ({ name, label, value, onChange }) => {
         type="text"
         className="form-control"
       />
+      {/* if the left side truthy the right side would appear otherwise nothing happen */}
+      {error && <div className="alret alert-danger">{error}</div>}
     </div>
   );
 };
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,9 +3,39 @@ import Input from "./common/input";
 
 class LoginForm extends Component {
   state = {
-    account: { username: "", password: "" }
+    account: { username: "", password: "" },
+    // it is easier to use object than array to find something(for array you need to use the find method and it's ugly)
+    // errors["username"] vs errors.find(e => e.name === "username")
+    errors: {} // this object would be populated with errors that reflect th account members(username:"type somsething")
   };
+
+  validate = () => {
+    const errors = {};
+    const { account } = this.state;
+
+    if (account.username.trim() === "")
+      errors.username = "Username is required";
+
+    if (account.password.trim() === "")
+      errors.password = "Password is required";
+
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
+  // we have two validator: one(validate) that validate the whole form
+  // and the validateProperty that validate each box(user or password individually)
+  validateProperty = ({ value, name }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is required";
+      // you can add more conditions like if is lass than 8 chactors
+    }
+  };
+
   handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
     account[input.name] = input.value; // use this to listen to the targeted element and get the value, set then names in forms
     this.setState({ account });
@@ -13,11 +43,20 @@ class LoginForm extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
+
+    const errors = this.validate();
+    console.log(errors);
+    // in the validate if everything is ok it would be null, but react doesn't like
+    // so either has to be an error or an ampty object
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
+    // call the server
     console.log("submitted");
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Log in</h1>
@@ -27,12 +66,14 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary">Login</button>
         </form>
